e111e40426520e2be527cec7663db229f242ac23
juacom99 <juacom04@gmail.com>
2015-04-22 01:50:44 -0300

    Emoticons working

--- src/com/hermes/common/AresFormater.java
+++ src/com/hermes/common/AresFormater.java
@@ -28,22 +28,22 @@ public class AresFormater
 
     public static final Color[] COLORS =
     {
-        new Color(248, 248, 248),//Blanco
-        Color.BLACK,//Negro
+        new Color(248, 248, 248),
+        Color.BLACK,
         new Color(0, 0, 128),
-        new Color(0, 128, 0),//verde oscuro
-        Color.RED,//Rojo
-        new Color(128, 0, 0),//rojo oscuro
-        new Color(128, 0, 128),//violeta
-        new Color(255, 128, 0),//naranja
-        new Color(255, 255, 0),//amarillo
-        Color.GREEN,//verde
-        new Color(0, 128, 128), //ocean green
-        new Color(0, 255, 255),//Cyan
+        new Color(0, 128, 0),
+        Color.RED,
+        new Color(128, 0, 0),
+        new Color(128, 0, 128),
+        new Color(255, 128, 0),
+        new Color(255, 255, 0),
+        Color.GREEN,
+        new Color(0, 128, 128),
+        new Color(0, 255, 255),
         Color.BLUE,
-        new Color(255, 0, 255),//Rosado
-        new Color(128, 128, 128),//gris oscuro
-        new Color(191, 191, 191) //gris claro
+        new Color(255, 0, 255),
+        new Color(128, 128, 128),
+        new Color(191, 191, 191)
     };
 
     private static AresFormater instance;
@@ -51,18 +51,53 @@ public class AresFormater
     private AresFormater()
     {
         emoticons.put(":-?\\)", "0.gif");
-
         emoticons.put(":-?d", "1.gif");
         emoticons.put(";-?\\)", "2.gif");
         emoticons.put(":-?o", "3.gif");
         emoticons.put(":-?p", "4.gif");
         emoticons.put("\\(h\\)", "5.gif");
-        emoticons.put("\\(6\\)", "6.gif");
+        emoticons.put(":-?@", "6.gif");
         emoticons.put(":-?\\$", "7.gif");
         emoticons.put(":-?s", "8.gif");
         emoticons.put(":-?\\(", "9.gif");
         emoticons.put(":'\\(", "10.gif");
-
+        emoticons.put(":-?\\|", "11.gif");
+        emoticons.put("\\(6\\)", "12.gif");
+        emoticons.put("\\(a\\)", "13.gif");
+        emoticons.put("\\(l\\)", "14.gif");
+        emoticons.put("\\(u\\)", "15.gif");
+        emoticons.put("\\(m\\)", "16.gif");
+        emoticons.put("\\(@\\)", "17.gif");
+        emoticons.put("\\(&\\)", "18.gif");
+        emoticons.put("\\(s\\)", "19.gif");
+        emoticons.put("\\(\\*\\)", "20.gif");
+        emoticons.put("\\(~\\)", "21.gif");
+        emoticons.put("\\(e\\)", "22.gif");
+        emoticons.put("\\(8\\)", "23.gif");
+        emoticons.put("\\(f\\)", "24.gif");
+        emoticons.put("\\(w\\)", "25.gif");
+        emoticons.put("\\(o\\)", "26.gif");
+        emoticons.put("\\(k\\)", "27.gif");
+        emoticons.put("\\(g\\)", "28.gif");
+        emoticons.put("\\(\\^\\)", "29.gif");
+        emoticons.put("\\(p\\)", "30.gif");
+        emoticons.put("\\(i\\)", "31.gif");
+        emoticons.put("\\(c\\)", "32.gif");
+        emoticons.put("\\(t\\)", "33.gif");
+        emoticons.put("\\(\\{\\)", "34.gif");
+        emoticons.put("\\(\\}\\)", "35.gif");
+        emoticons.put("\\(b\\)", "36.gif");
+        emoticons.put("\\(d\\)", "37.gif");
+        emoticons.put("\\(z\\)", "38.gif");
+        emoticons.put("\\(x\\)", "39.gif");
+        emoticons.put("\\(y\\)", "40.gif");
+        emoticons.put("\\(n\\)", "41.gif");
+        emoticons.put(":-?\\[", "42.gif");
+        emoticons.put("\\(1\\)", "43.gif");
+        emoticons.put("\\(2\\)", "44.gif");
+        emoticons.put("\\(3\\)", "45.gif");
+        emoticons.put("\\(4\\)", "46.gif");
+        
     }
 
     public String toHTML(String s)
@@ -102,7 +137,7 @@ public class AresFormater
         {
             color = "#" + Integer.toHexString(COLORS[Integer.parseInt(matcher.group(1))].getRGB()).substring(2);
             text = matcher.group(2);
-            matcher.appendReplacement(sb, "<span style=\"background-color:" + color + "\">" + text + "<span/>");
+            matcher.appendReplacement(sb, "<span style=\"background-color:" + color + "\">" + text);
 
         }
         matcher.appendTail(sb);
@@ -124,7 +159,7 @@ public class AresFormater
         {
             color = "#" + Integer.toHexString(COLORS[Integer.parseInt(matcher.group(1))].getRGB()).substring(2);
             text = matcher.group(2);
-            matcher.appendReplacement(sb, "<span style=\"color:" + color + "\">" + text + "<span/>");
+            matcher.appendReplacement(sb, "<span style=\"color:" + color + "\">" + text);
         }
         matcher.appendTail(sb);
         return sb.toString();
@@ -224,7 +259,7 @@ public class AresFormater
         Iterator<String> i = emoticons.keySet().iterator();
         String ret = str;
         String key;
-        String emoticonsFolder = (System.getProperty("user.dir") + File.separator + "emoticons" + File.separator).replace(File.separator, File.separator + "" + File.separator);
+        String emoticonsFolder = (System.getProperty("user.dir") + File.separator + "emoticons" + File.separator).replace("\\", "\\\\");
         while (i.hasNext())
         {
             key = i.next();
